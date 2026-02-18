e4955a69daa0aa82acafbbb39e891612610f72dd
Daniel <danielparientegarcia@elpuig.xeill.net>
2026-02-18 19:50:36 +0100

    Interfaz pagina web mejorada, tanto la pagina de inicio como la de estadisticas

--- js/app.js
+++ js/app.js
@@ -1,2 +1,13 @@
 console.log("SafeGoalStats cargado correctamente");
 alert("Bienvenido a SafeGoalStats");
+
+function ordenarTabla() {
+  const tabla = document.getElementById("tablaLiga");
+  const filas = Array.from(tabla.rows).slice(1);
+
+  filas.sort((a, b) => {
+    return b.cells[5].innerText - a.cells[5].innerText;
+  });
+
+  filas.forEach(fila => tabla.appendChild(fila));
+}

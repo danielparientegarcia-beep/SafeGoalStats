f4d7f81f2c815f5ba38ab426f4e32e3e5e71c98f
Daniel <danielparientegarcia@elpuig.xeill.net>
2026-04-10 17:38:26 +0200

    cambios y base de datos operativa

---
+++ js/registro.js
@@ -0,0 +1,32 @@
+const form = document.getElementById('registroForm');
+const mensaje = document.getElementById('mensaje');
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const username = document.getElementById('username').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  try {
+    const res = await fetch('http://192.168.239.119:3000/api/auth/registro', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, email, password })
+    });
+
+    const data = await res.json();
+
+    if (res.ok) {
+      mensaje.textContent = ' ✅ Usuario registrado correctamente';
+      form.reset();
+    } else {
+      mensaje.textContent = '❌ ' + data.error;
+    }
+
+  } catch (error) {
+    mensaje.textContent = '❌ Error de conexión con el servidor';
+  }
+});

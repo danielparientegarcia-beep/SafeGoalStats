9efb59cfec7f6eb56af8e16b90327c906cb0e632
Daniel <danielparientegarcia@elpuig.xeill.net>
2026-04-15 19:48:47 +0200

    se agrega la pagina de login, le faltan muchos cambios

---
+++ js/login.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const form = document.getElementById("form-login");
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    try {
+
+      const res = await fetch("http://192.168.239.119:3000/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ email, password })
+      });
+
+      const data = await res.json();
+
+      if (res.ok) {
+
+        localStorage.setItem("token", data.token);
+
+        alert("Login correcto");
+
+        window.location.href = "/safegoalstats/index.html";
+
+      } else {
+        alert(data.mensaje || "Error en login");
+      }
+
+    } catch (error) {
+      console.error(error);
+      alert("Error de conexión con el servidor");
+    }
+
+  });
+
+});

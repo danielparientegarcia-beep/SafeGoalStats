b28946050e0e04e640605c9aa5fdf7f053343836
Daniel <danielparientegarcia@elpuig.xeill.net>
2026-05-05 19:29:02 +0200

    Hemos implementado la api a toda la web para los datos sobre futbol

--- js/auth.js
+++ js/auth.js
@@ -1,40 +1,56 @@
+/*
+=========================
+AUTH.JS
+Control de sesión
+=========================
+*/
+
+
 // Obtener token guardado
 function getToken() {
   return localStorage.getItem("token");
 }
 
-// Comprobar si está logueado
+
+// Saber si hay sesión iniciada
 function isLogged() {
   return !!localStorage.getItem("token");
 }
 
+
 // Cerrar sesión
 function logout() {
   localStorage.removeItem("token");
-  window.location.href = "/safegoalstats/pages/login.html";
+  window.location.href = "/safegoalstats/index.html";
 }
+
+
+// Pintar menú Login / Registro / Logout
 window.addEventListener("load", () => {
 
   const navAuth = document.getElementById("nav-auth");
+
   if (!navAuth) return;
 
-  const token = localStorage.getItem("token");
+  const token = getToken();
 
   if (token) {
+
     navAuth.innerHTML = `
       <a href="#" id="logout-btn" class="registro">Logout</a>
     `;
 
-    document.getElementById("logout-btn").addEventListener("click", () => {
-      localStorage.removeItem("token");
-      window.location.href = "/safegoalstats/index.html";
-    });
+    document
+      .getElementById("logout-btn")
+      .addEventListener("click", logout);
 
   } else {
-   navAuth.innerHTML = `
-  <a href="/safegoalstats/pages/login.html" class="registro">Login</a>
-  <a href="/safegoalstats/pages/registro.html" class="registro">Registro</a>
-`;
+
+    navAuth.innerHTML = `
+      <a href="/safegoalstats/pages/login.html" class="registro">Login</a>
+      <a href="/safegoalstats/pages/registro.html" class="registro">Registro</a>
+    `;
+
   }
 
 });
